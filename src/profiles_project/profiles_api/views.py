a0f121e4f85ee44266da9fe96b01639587083eee
ElenaPrueba <ola397@gmail.com>
2019-08-13 09:32:22 +0200

    Viewset

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from . import serializers
+
+#Lista de respuestas HTTP
+from rest_framework import status
+
+
+from rest_framework import viewsets
+
+
 # Create your views here.
 class HelloApiView(APIView):
     """Test API View"""
+
+
+    serializers_class = serializers.HelloSerializer
 
     def get(self, request,format=None):
         """Devuelve una lista de las caracteristicas de APIView"""
@@ -15,3 +27,45 @@ class HelloApiView(APIView):
                     'Está mapeada con URLs'
                     ]
         return Response({'mensaje': 'Hola', 'an_apiview':an_apiview})
+
+    def post(self, request):
+        """Crea un saludo con nuestro nombre"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handles Actualizando un objeto."""
+
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        """Patch request, solo actualiza los campos proporcionados en la solicitud."""
+
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None): #pk = primary key
+        """Borra un objeto."""
+
+        return Response({'method': 'delete'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+        """Devuelve un mensaje de hola."""
+
+        a_viewset = [
+            'Usa acciones (list, create, retrieve, update, partial_update)',
+            'Automaticamente mapea URLS usando Routers',
+            'Proporciona mas funcionalidad con menos codigo'
+        ]
+
+        return Response({'message': 'Hola!', 'a_viewset': a_viewset})
