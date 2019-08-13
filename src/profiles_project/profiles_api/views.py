fd0f6d0f0472f3f636c4ee0ec560fe4ede31e8c0
ElenaPrueba <ola397@gmail.com>
2019-08-13 11:38:52 +0200

    Buscador por nombre y email

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -9,6 +9,8 @@ from rest_framework import viewsets
 
 from rest_framework.authentication import TokenAuthentication
 
+from rest_framework import filters
+
 from . import serializers
 from . import models
 from . import permissions
@@ -120,4 +122,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
-    
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
